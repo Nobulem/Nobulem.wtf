e0cd1954aee4b58ff6cea4f12795d6c6af612247
nobulem.wtf <nobulemv2@gmail.com>
2025-10-28 11:55:28 +0300

    Update notiflib.lua

--- notiflib.lua
+++ notiflib.lua
@@ -1,8 +1,6 @@
 local cloneref = cloneref or function(obj) return obj end
 
 local CoreGui = cloneref(game:GetService("CoreGui"))
-local TweenService = cloneref(game:GetService("TweenService"))
-
 local ScreenGui = Instance.new("ScreenGui")
 ScreenGui.Name = "FatalityHitLogs"
 ScreenGui.IgnoreGuiInset = true
@@ -17,43 +15,49 @@ Holder.BackgroundTransparency = 1
 Holder.Parent = ScreenGui
 
 local Layout = Instance.new("UIListLayout")
-Layout.SortOrder = Enum.SortOrder.LayoutOrder
-Layout.Padding = UDim.new(0, 4)
-Layout.VerticalAlignment = Enum.VerticalAlignment.Top
 Layout.Parent = Holder
+Layout.SortOrder = Enum.SortOrder.LayoutOrder
+Layout.VerticalAlignment = Enum.VerticalAlignment.Bottom
+Layout.Padding = UDim.new(0, 4)
 
 local NotificationModule = {}
+local ActiveNotifs = {}
 
 function NotificationModule.Notify(text, duration)
-	duration = duration or 3
+    duration = duration or 3
 
-	local label = Instance.new("TextLabel")
-	label.Size = UDim2.new(1, 0, 0, 20)
-	label.BackgroundTransparency = 1
-	label.Text = string.lower(text)
-	label.Font = Enum.Font.GothamSemibold
-	label.TextSize = 16
-	label.TextColor3 = Color3.fromRGB(255, 255, 255)
-	label.TextTransparency = 1
-	label.TextStrokeTransparency = 1
-	label.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
-	label.TextXAlignment = Enum.TextXAlignment.Right
-	label.Parent = Holder
-	local fadeIn = TweenService:Create(label, TweenInfo.new(0.2), {
-		TextTransparency = 0,
-		TextStrokeTransparency = 0.4
-	})
-	fadeIn:Play()
+    local label = Instance.new("TextLabel")
+    label.Parent = Holder
+    label.Size = UDim2.new(1, 0, 0, 20)
+    label.BackgroundTransparency = 1
+    label.Text = string.lower(text)
+    label.Font = Enum.Font.GothamSemibold
+    label.TextSize = 16
+    label.TextColor3 = Color3.fromRGB(255, 255, 255)
+    label.TextTransparency = 1
+    label.TextStrokeTransparency = 1
+    label.TextStrokeColor3 = Color3.fromRGB(0, 0, 0)
+    label.TextXAlignment = Enum.TextXAlignment.Right
 
-	task.delay(duration, function()
-		local fadeOut = TweenService:Create(label, TweenInfo.new(0.3), {
-			TextTransparency = 1,
-			TextStrokeTransparency = 1
-		})
-		fadeOut:Play()
-		fadeOut.Completed:Wait()
-		label:Destroy()
-	end)
+    table.insert(ActiveNotifs, label)
+
+    task.spawn(function()
+        for i = 1, 10 do
+            label.TextTransparency = 1 - (i / 10)
+            label.TextStrokeTransparency = 1 - (i / 10) * 0.6
+            task.wait(0.02)
+        end
+
+        task.wait(duration)
+        for i = 1, 10 do
+            label.TextTransparency = i / 10
+            label.TextStrokeTransparency = i / 10
+            task.wait(0.02)
+        end
+
+        label:Destroy()
+        table.remove(ActiveNotifs, table.find(ActiveNotifs, label))
+    end)
 end
 
 return NotificationModule
